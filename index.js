9f6a60d62a16be1409be001b8cca94ca4c726029
haeburney <76997276+haeburney@users.noreply.github.com>
2022-07-10 21:28:07 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const express = require('express') //express modules를 가져온다.
-const app = express() //function을 이용해서 새로운 express 앱을 만들고
-const port = 5000 //4000 해도 되고, 5000번 해도 된다.
+const express = require('express') 
+const app = express() 
+const port = 5000 
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hyewonkim:kim2841@cluster0.38bvqlk.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   //useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! backend로 run')
+})
+
+app.post('/register', (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+  
+  //정보를 데이터베이스에 넣기 위해서는 
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success:true
+    })
+  }) 
+  //몽고DB 메서드 저장이 된 것이다.
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
